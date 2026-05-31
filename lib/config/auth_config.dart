78a26eb994beff88d4f5d4304ad5eb6e5f22bd3a
Kaluma-Machanisse <kalumaa815@gmail.com>
2026-05-31 18:43:52 +0200

    Remover credenciais do versionamento e atualizar .gitignore

--- lib/config/auth_config.dart
+++
@@ -1,5 +0,0 @@
-class AuthConfig {
-  // Substitui pelos dados que usaste ao criar o utilizador no Supabase
-  static const String email = 'kalumaa815@gmail.com';
-  static const String password = 'Otalkaluminha1';
-}
--- lib/config/supabase_config.dart
+++
@@ -1,4 +0,0 @@
-class SupabaseConfig {
-  static const String url = 'https://kmkcytjbmrbbhqfyycmg.supabase.co';
-  static const String anonKey = 'sb_publishable_NYMLSNcIoobBL7OnhoXEMg_NsJslygz';
-}
